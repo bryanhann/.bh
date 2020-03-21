bada916cb8a27ba0103ea452b079821495fb9365
Bryan Hann <nobody@nowhere>
2020-03-21 11:41:57 +0800

    Add dunders installer. This must be installed before shell.

---
+++ sbin/installers/dunders.sh
@@ -0,0 +1,4 @@
+SRC=https://github.com/bryanhann/bch.dunders
+DST=${HOME}/.local/bch/dunders
+[ -d ${DST} ] || git clone ${SRC} ${DST}
+
--- sbin/installers/shell.sh
+++ sbin/installers/shell.sh
@@ -1,14 +1,7 @@
-echo "Your dotfiles will be clobbered. Continue? [y/N] \c "; read X
-{ [ "$X" == "y" ] || [ "$X" == "Y" ]; } || exit;
-
-HERE=$(dirname $(grealpath $0))
 SRC=https://github.com/bryanhann/bch.shell
-
-[ -z "$1"   ] && DST=${HERE}/shell.repo || DST=$1
-[ -d ${DST} ] && echo Already cloned $DST || git clone ${SRC} ${DST}
-
+DST=${HOME}/.local/bch/shell
+[ -d ${DST} ] || git clone ${SRC} ${DST}
 echo Clobbering dot files.
-
 # The bash ~/.dotfiles must call their dot.files with
 # the dot.file folder folder as a first argument. The
 # zsh dotfiles do not need to do this.``
