e317ed2af76cb00b349f5a350d01124b2968e407
Bryan Hann <nobody@nowhere>
2020-03-29 17:24:54 +0800

    Fix faulty name.

--- zprofile.sh
+++ zprofile.sh
@@ -4,7 +4,7 @@ _export () {export ${1}=${2};}
 
 _export HERE                $(dirname $(grealpath ${0}))
 _export ZDOTDIR             ${HERE}
-_export ZDOTBLD             ~/.local/zhbsh
+_export ZDOTBLD             ~/.local/bhzsh
 _export XDG_CONFIG_HOME     ${HOME}/.config
 _export XDG_CACHE_HOME      ${HOME}/.cache
 _export PIP_DOWNLOAD_CACHE  ${HOME}/.cache/pip
