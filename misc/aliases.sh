4f4ca4ad59171975117384a8e351cdfecb7fdba4
Bryan Hann <nobody@nowhere>
2020-03-30 19:11:21 +0800

    Replace local alias with r20.activate.bch

--- misc/aliases.sh
+++
@@ -1,4 +0,0 @@
-alias vlc=/Applications/VLC.app/Contents/MacOS/VLC
-alias .z='zjot new'
-alias ls='ls -F'
-alias xx='chmod +x'
--- zshrc.sh
+++ zshrc.sh
@@ -1,6 +1,5 @@
 #echo ++ zshrc.sh $0
 source $(dirname $(grealpath $0))/init.sh
-source ${HERE}/misc/aliases.sh
 source ${HERE}/env/vwrap.sh
 source ${HERE}/env/omzsh.sh
 _r20 dunders
@@ -8,4 +7,5 @@ _r20 venv
 _r20 zjot
 _r20 vwrap
 _r20 omzsh
+_r20 bch
 #echo -- zshrc.sh
