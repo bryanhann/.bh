b5f43cd6c2cc38ca7a0707095aaaab155ff72f49
Bryan Hann <nobody@nowhere>
2020-03-30 18:50:54 +0800

    Update configuration

--- env/omzsh.sh
+++ env/omzsh.sh
@@ -1,2 +1,13 @@
+#ZSH_THEME="robbyrussell"   # frisk
+#ZSH_THEME="sunrise"        # shows last two components of cdw
+#ZSH_THEME="smt"            # chinese
+#ZSH_THEME="muse"           # light
+#ZSH_THEME="agnoster"       #
+#ZSH_THEME="fino-time"      #
+#ZSH_THEME="gnzh"           # based on bira. very good two liner. Very clear full cwd.
+#ZSH_THEME="bira"           #
+#ZSH_THEME="random"         #
+
 export ZSH_THEME=gnzh
-export plugins=(git)
+export plugins=(git git-prompt virtualenv virtualenvwrapper)
+
