1cb03d96e191aac0008f7763f06173a82363f8b5
Bryan Hann <nobody@nowhere>
2020-07-20 15:01:09 +0800

    Refactor venv and depth into external files.

--- zshrc.sh
+++ zshrc.sh
@@ -8,27 +8,8 @@ _r20_show   R20_INIT_BLD
 _r20_show   R20_INIT_URL
 _r20_show   R20_ZPROFILE_TMP
 _r20_show   R20_ZPROFILE_BIN
-
-##### DEPTH #####
-_r20_export R20_DEPTH $(( ${R20_DEPTH} + 1 ))
-function exit() { [[ "${R20_DEPTH}" == "0" ]] && echo try 'builtin exit' || builtin exit }
-function depth() { echo ${R20_DEPTH} }
-
-export dst=$R20/__BUILD__/VENV
-if [ ! -d $dst ];
-    then
-        python3 -m venv $dst
-        source $dst/bin/activate
-        pip install --upgrade pip
-        r20pip_install virtualenv
-        r20pip_install virtualenvwrapper
-        deactivate
-    else
-fi
-
-source $dst/bin/activate
-source $dst/bin/virtualenvwrapper.sh
-
+source $R20/depth.sh
+source $R20/venv.sh
 r20install  dunders #-r
 r20install  bch
 r20install  zjot
