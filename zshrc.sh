6f47ffa78071361c8490b0d850eec36259cbc8fe
Bryan Hann <nobody@nowhere>
2020-07-20 14:54:53 +0800

    Internalize venv and vwrap

--- zshrc.sh
+++ zshrc.sh
@@ -14,13 +14,26 @@ _r20_export R20_DEPTH $(( ${R20_DEPTH} + 1 ))
 function exit() { [[ "${R20_DEPTH}" == "0" ]] && echo try 'builtin exit' || builtin exit }
 function depth() { echo ${R20_DEPTH} }
 
+export dst=$R20/__BUILD__/VENV
+if [ ! -d $dst ];
+    then
+        python3 -m venv $dst
+        source $dst/bin/activate
+        pip install --upgrade pip
+        r20pip_install virtualenv
+        r20pip_install virtualenvwrapper
+        deactivate
+    else
+fi
+
+source $dst/bin/activate
+source $dst/bin/virtualenvwrapper.sh
+
 r20install  dunders #-r
-r20install  venv #-r
 r20install  bch
 r20install  zjot
 r20install  omzsh
 r20install  libre
-r20install  vwrap #-r
 r20install  ws852
 _r20_exit
 #_colors
