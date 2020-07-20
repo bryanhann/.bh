e8152f117acd87cf86c9a36cba1a9563840146b6
Bryan Hann <nobody@nowhere>
2020-07-20 15:55:11 +0800

    Internalize zjot

--- zshrc.sh
+++ zshrc.sh
@@ -8,13 +8,12 @@ _r20_show   R20_INIT_BLD
 _r20_show   R20_INIT_URL
 _r20_show   R20_ZPROFILE_TMP
 _r20_show   R20_ZPROFILE_BIN
+source $R20/modules/zjot/activate.sh
 source $R20/modules/depth/activate.sh
 source $R20/modules/venv/activate.sh
 source $R20/modules/dunders/activate.sh
 source $R20/modules/bch/activate.sh
 source $R20/modules/ws852/activate.sh
-#r20install  zjot
-#r20install  omzsh
-#r20install  libre
+r20install  omzsh
 _r20_exit
 #_colors
