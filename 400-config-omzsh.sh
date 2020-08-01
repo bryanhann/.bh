5a3cc0ad297475be61f498dc9e7e9252bdbd2f1d
Bryan Hann <nobody@nowhere>
2020-08-01 15:22:31 +0800

    Tweak omzsh configuration.

--- 400-config-omzsh.sh
+++ 400-config-omzsh.sh
@@ -1,3 +1,13 @@
-ZSH_THEME="minimal" # fox minimal kiwi "bira"
-plugins=(git)
+# minimal : very good
+# virtualenv_prompt_info
+# git_prompt_info
+# minimal fox  kiwi bira steef dallas kafeitu(nice)
+# nanotech : wide, 2level
+# suvash : very good
+# essembeh gozilla sonicradish juanghurtado[!] wedisagree : rich git info
+# jaischeema : pumpkin at ~/borg-dev ±(dev) ✗ ❯
+# strug : origin/dev
+
+ZSH_THEME=kafeitu # plugins must not contain virtualenv for prompt to show virtual_env)
+plugins=(git virtualenvwrapper)
 source $ZSH/oh-my-zsh.sh
