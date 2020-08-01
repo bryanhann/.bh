43097a3144dc46520872e2858eedd5f3d4291dd6
Bryan Hann <nobody@nowhere>
2020-08-01 14:55:20 +0800

    Fix latent error with virtualenv
    Issue 1. The vital file 'virtualenv' contains a hard link to a python
    installation. So on every shell startup we will (i) create a python
    installation at [$BORGVENV] and [pip install virtualenv]
    The file 'virtualenv'
       i) points to a python installation via a hard link
       ii) must be on PATH.
    
    To accomodate this,
       * we maintain a fixed python installation with virtualenv in
          the foldthe build folder.
       ii) we ci

--- 200-install-python.sh
+++ 200-install-python.sh
@@ -1,28 +1,13 @@
 [[ -d $BORGVENV ]] || {
     echo installing python venv
-    rm -rf $BORGVENV
     python3 -m venv $BORGVENV
     source $BORGVENV/bin/activate
-    # here we expect internet connectivity:
-    pip install --upgrade pip
     pip install virtualenv
     pip install virtualenvwrapper
-    # here we copy missing files in case of no internet:
-    # (user should do an internet reinstall later!)
-    cp -n $BORGHOME/vendor/venv/* $BORGVENV/bin
+    deactivate
 }
-
-.workon () {
-    if [[ "$1" == "borg" ]]; then
-        source ${BORGVENV}/bin/activate
-        source virtualenvwrapper.sh
-    else
-        workon $*
-    fi
-}
-
+cp $BORGVENV/bin/v* $BORGBIN
+source virtualenvwrapper.sh
 .remove-python () {
     rm -rf $BORGVENV
 }
-
-.workon borg
