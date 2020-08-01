bc01f4e0cb3fff7c5e70dedb5865300dbfdf555d
Bryan Hann <nobody@nowhere>
2020-08-01 14:32:57 +0800

    Externalize .build and normalize envnames.

--- 000-initiate.sh
+++ 000-initiate.sh
@@ -1,12 +1,17 @@
 [[ -z ${BORGHOME} ]] && {
-    .export BORGHOME  $ZDOTDIR
+    .export BORGHOME   $ZDOTDIR
+    .export PYTHONPATH $BORGHOME/lib/python
+
+    .export BORGBUILD  ~/.build
+    .export BORGTEMP   $BORGBUILD/BORGTMP
+    .export ZSH        $BORGBUILD/ZSH
+    .export BORGBIN    $BORGBUILD/BORGBIN
+    .export BORGVENV   $BORGBUILD/BORGVENV
+
+    .export PATH       $PATH:$BORGBIN
     .export BORGDEPTH -1
     .export BORGPID0  $$
-    .export BORGBUILD $BORGHOME/.build
-    .export BORGTEMP  $BORGHOME/.tmp
-    .export BORGVENV  $BORGBUILD/venv
-    .export ZSH       $BORGBUILD/omzsh
-    .export PATH      $PATH:$BORGHOME/bin
-    .export PYTHONPATH ${PYTHONPATH}:${BORGHOME}/lib/python
+
     mkdir -p $BORGTEMP
+    mkdir -p $BORGBIN
 }
