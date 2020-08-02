86202e7a7b69d95d34fe6b70c234bbf6ab2fab51
Bryan Hann <nobody@nowhere>
2020-08-02 14:54:09 +0800

    Merge 010 into 210

--- 210-activate-python-environment.sh
+++ 210-activate-python-environment.sh
@@ -1,2 +1,6 @@
+.export WORKON_HOME                 $BORGBUILD/WORKON_HOME
+.export PROJECT_HOME                $BORGBUILD/PROJECT_HOME
+.export VIRTUALENVWRAPPER_HOOK_DIR  $BORGBUILD/VIRTUALENVWRAPPER_HOOK_DIR
+
 .note the next line is redundant if omzsh is using plugin \'virtualenvwrapper\'
 .source virtualenvwrapper.sh
