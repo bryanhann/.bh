9c5c12cd82321653d1006ace4f2e4e170291c806
Bryan Hann <nobody@nowhere>
2020-08-02 15:05:52 +0800

    Remove unnecessary conditional statement

--- 000-initiate.sh
+++ 000-initiate.sh
@@ -1,18 +1,16 @@
-[[ -z ${BORG} ]] && {
-    .export BORG       $ZDOTDIR
-    .export BORGBUILD  ~/.build
+.export BORG       $ZDOTDIR
+.export BORGBUILD  ~/.build
 
-    .export BORGTEMP   $BORGBUILD/BORGTMP
-    .export BORGBIN    $BORGBUILD/BORGBIN
-    .export BORGVENV   $BORGBUILD/BORGVENV
+.export BORGTEMP   $BORGBUILD/BORGTMP
+.export BORGBIN    $BORGBUILD/BORGBIN
+.export BORGVENV   $BORGBUILD/BORGVENV
 
-    .export BORGDEPTH -1
-    .export BORGPID0  $$
+.export BORGDEPTH -1
+.export BORGPID0  $$
 
-    mkdir -p $BORGTEMP
-    mkdir -p $BORGBIN
+mkdir -p $BORGTEMP
+mkdir -p $BORGBIN
 
-    .export PATH       $PATH:$BORGBIN
-    .export ZSH        $BORGBUILD/ZSH
-    .export PYTHONPATH $BORG/lib/python
-}
+.export PATH       $PATH:$BORGBIN
+.export ZSH        $BORGBUILD/ZSH
+.export PYTHONPATH $BORG/lib/python
