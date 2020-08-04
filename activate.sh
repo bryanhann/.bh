d7ef9e431d8584762007da5435f5d2effb788431
Bryan Hann <nobody@nowhere>
2020-08-05 06:03:25 +0800

    Incorporate [functions.sh] into activate.sh

--- activate.sh
+++ activate.sh
@@ -1,7 +1,27 @@
+#======================================================================
+# Functions to make display of installation colorful and nested
+#======================================================================
+
+export NEST=0
+_up      () {  export NEST=$((${NEST}+1)); }
+_down    () {  export NEST=$((${NEST}-1)); }
+_tab     () { [[ "${NEST}" == "0" ]] || for i in $( seq ${NEST} ); do printf '\\t'; done ; }
+_color   () { tput setaf $1; [[ "$2" == "." ]] || tput setab $2; shift; shift; echo -n $*; tput sgr0; }
+_black   () { _color 15  0 $(_tab)$*; echo; }
+_blue    () { _color 15  4 $(_tab)$*; echo; }
+_yellow  () { _color 16 11 $(_tab)$*; echo; }
+_green   () { _color  0 10 $(_tab)$*; echo; }
+_red     () { _color 15  9 $(_tab)$*; echo; }
+.note () { _green "# $*" }
+.exec () { _red exec: $*; $*; }
+.export () { _blue "setting \$$1 := [$2]" ; export $1=$2 }
+.source () { _yellow "++[. $*]" ; _up ; source $* ; _down ; _yellow "--[. $*]"; }
+
+
 # Assume this directory is $ZDOTDIR
 
-cd $ZDOTDIR
-source functions.sh
+#cd $ZDOTDIR
+#source functions.sh
 
 [[ -z "$BORG" ]] && {
     .export BORG $ZDOTDIR
