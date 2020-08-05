9d1d5e6d03c3c79380c029362eae14b4c06f6629
Bryan Hann <nobody@nowhere>
2020-08-05 18:49:35 +0800

    Refactor hard coded values to $BORG_LOCAL

--- activate.sh
+++ activate.sh
@@ -16,19 +16,21 @@ function main () {
 profile () {
     [[ -z "$BORG" ]] || return
     .export BORG $ZDOTDIR
-    .export BORG_VENV                   ~/.local/borg/VENV
-    .export ZSH                         ~/.local/borg/ZSH
-    .export BORG_REPOS                  ~/.local/borg/BORG_REPOS
-    .export WORKON_HOME                 ~/.local/borg/WORKON_HOME
-    .export PROJECT_HOME                ~/.local/borg/PROJECT_HOME
-    .export VIRTUALENVWRAPPER_HOOK_DIR  ~/.local/borg/VIRTUALENVWRAPPER_HOOK_DIR
+    .export BORG_LOCAL                  ~/.local/borg
+    .export BORG_VENV                   ${BORG_LOCAL}/VENV
+    .export ZSH                         ${BORG_LOCAL}/ZSH
+    .export BORG_REPOS                  ${BORG_LOCAL}/BORG_REPOS
+    .export WORKON_HOME                 ${BORG_LOCAL}/WORKON_HOME
+    .export PROJECT_HOME                ${BORG_LOCAL}/PROJECT_HOME
+    .export VIRTUALENVWRAPPER_HOOK_DIR  ${BORG_LOCAL}/VIRTUALENVWRAPPER_HOOK_DIR
     .export VIRTUALENVWRAPPER_PYTHON    ${BORG_VENV}/bin/python
-    .export HISTFILE                    ~/.local/borg/HISTFILE
     mkdir -p $WORKON_HOME
     mkdir -p $PROJECT_HOME
     mkdir -p $BORG_REPOS
     .export PATH            $PATH:$BORG_VENV/bin
     .export PYTHONPATH      $BORG/lib/python
+
+    .export HISTFILE                    ${BORG_LOCAL}/HISTFILE
     .export HISTSIZE    5000            # How many lines of history to keep in memory
     .export SAVEHIST    5000            # Number of history entries to save to disk
     .export HISTDUP     erase           # Erase duplicates in the history file
