2dded19bde72b86b26f78b3f23eb21a4d504d6f6
Bryan Hann <nobody@nowhere>
2020-08-05 20:52:55 +0800

    Incorporate sourced code back into [activate.sh]

--- activate.sh
+++ activate.sh
@@ -1,66 +1,49 @@
-source $ZDOTDIR/functions.sh
+function venv.configure {
+    .exec pip install --upgrade pip
+    .exec pip install virtualenv
+    .exec pip install virtualenvwrapper
+    .exec pip install cat-in-a-box
+}
 
-#======================================================================
-#  MAIN
-#======================================================================
+function omzsh.configure {
+    .export ZSH_THEME kafeitu # plugins must not contain virtualenv for prompt to show virtual_env)
+    plugins=(git virtualenvwrapper)
+}
+
 function main () {
     .call profile
     .call venv
     .call omzsh
     .source ${ZDOTDIR}/history.sh
+    [[ -z "$BHPID" ]] && .export BHPID $$
 }
 
-#======================================================================
-#  PROFILE
-#======================================================================
-profile () {
+function profile () {
     [[ -z "$BORG" ]] || return
     .export BORG $ZDOTDIR
     .export BORG_LOCAL      ~/.local/borg
     .export BORG_VENV       ${BORG_LOCAL}/VENV
     .export PATH            $PATH:$BORG_VENV/bin
     .export PYTHONPATH      $ZDOTDIR/lib/python
-}
-
-#======================================================================
-#  VENV
-#======================================================================
-function venv.install       { .note installing venv; .exec python3 -m venv $BORG_VENV; }
-function venv.remove        { .note removing venv; rm -rf $BORG_VENV; }
-function venv.activate      { .exec source $BORG_VENV/bin/activate }
-function venv.configure     { .source $ZDOTDIR/venv.config.sh }
-function venv.deactivate    { .exec deactivate }
-function venv.prepare       {
-    .export WORKON_HOME                 ${BORG_LOCAL}/WORKON_HOME
-    .export PROJECT_HOME                ${BORG_LOCAL}/PROJECT_HOME
     .export VIRTUALENVWRAPPER_HOOK_DIR  ${BORG_LOCAL}/VIRTUALENVWRAPPER_HOOK_DIR
     .export VIRTUALENVWRAPPER_PYTHON    ${BORG_VENV}/bin/python
+    .export WORKON_HOME                 ${BORG_LOCAL}/WORKON_HOME
+    .export PROJECT_HOME                ${BORG_LOCAL}/PROJECT_HOME
     .exec mkdir -p $PROJECT_HOME # vwrap does not create it automatically
-}
-function venv () {
-    venv.prepare
-    venv.install        # .exec python3 -m venv $BORG_VENV;
-    venv.activate       #.exec source $BORG_VENV/bin/activate
-    venv.configure      #.source $ZDOTDIR/venv.update.sh
-    venv.deactivate     #.exec deactivate
-}
-
-#======================================================================
-#  OMZSH
-#======================================================================
-function omzsh.install  { $BORG/vendor/omzsh/install.sh --unattended; }
-function omzsh.remove   { .note removing omzsh; rm -rf $ZSH; }
-function omzsh.activate { .source $ZSH/oh-my-zsh.sh }
-function omzsh.configure   { .source $ZDOTDIR/omzsh.config.sh }
-function omzsh.prepare  {
     .export ZSH ${BORG_LOCAL}/ZSH
 }
-function omzsh () {
-    omzsh.prepare
-    omzsh.install
-    omzsh.configure
-    omzsh.activate
-}
 
+function venv { venv.install; venv.activate; venv.configure; venv.deactivate; }
+function venv.install { .note installing venv; .exec python3 -m venv $BORG_VENV; }
+function venv.remove { .note removing venv; rm -rf $BORG_VENV; }
+function venv.activate { .exec source $BORG_VENV/bin/activate; }
+function venv.deactivate { .exec deactivate; }
+
+function omzsh { omzsh.install; omzsh.configure; omzsh.activate; }
+function omzsh.install { $BORG/vendor/omzsh/install.sh --unattended; }
+function omzsh.remove { .note removing omzsh; rm -rf $ZSH; }
+function omzsh.activate { .source $ZSH/oh-my-zsh.sh }
+
+source $ZDOTDIR/functions.sh
 
 .call main
