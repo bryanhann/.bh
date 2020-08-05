5655aaafadd2b4bc16f4b8d6f3f7bd54a366642a
Bryan Hann <nobody@nowhere>
2020-08-05 19:05:52 +0800

    Factor setting of env vars into the code that uses them.

--- activate.sh
+++ activate.sh
@@ -4,10 +4,10 @@ source $ZDOTDIR/functions.sh
 #  MAIN
 #======================================================================
 function main () {
-    .call   profile
-    .call   venv
-    #.source virtualenvwrapper.sh
-    .call   omzsh
+    .call profile
+    .call venv
+    .call omzsh
+    .call repos
     .source ${ZDOTDIR}/history.sh
 }
 
@@ -17,25 +17,29 @@ function main () {
 profile () {
     [[ -z "$BORG" ]] || return
     .export BORG $ZDOTDIR
-    .export BORG_LOCAL                  ~/.local/borg
-    .export BORG_VENV                   ${BORG_LOCAL}/VENV
-    .export ZSH                         ${BORG_LOCAL}/ZSH
-    .export BORG_REPOS                  ${BORG_LOCAL}/BORG_REPOS
-    .export WORKON_HOME                 ${BORG_LOCAL}/WORKON_HOME
-    .export PROJECT_HOME                ${BORG_LOCAL}/PROJECT_HOME
-    .export VIRTUALENVWRAPPER_HOOK_DIR  ${BORG_LOCAL}/VIRTUALENVWRAPPER_HOOK_DIR
-    .export VIRTUALENVWRAPPER_PYTHON    ${BORG_VENV}/bin/python
-    mkdir -p $WORKON_HOME
-    mkdir -p $PROJECT_HOME
-    mkdir -p $BORG_REPOS
+    .export BORG_LOCAL      ~/.local/borg
+    .export BORG_VENV       ${BORG_LOCAL}/VENV
     .export PATH            $PATH:$BORG_VENV/bin
-    .export PYTHONPATH      $BORG/lib/python
+    .export PYTHONPATH      $ZDOTDIR/lib/python
+}
+
+#======================================================================
+#  REPOS
+#======================================================================
+repos () {
+    mkdir -p $BORG_REPOS
 }
 
 #======================================================================
 #  VENV
 #======================================================================
 function venv () {
+    .export WORKON_HOME                 ${BORG_LOCAL}/WORKON_HOME
+    .export PROJECT_HOME                ${BORG_LOCAL}/PROJECT_HOME
+    .export VIRTUALENVWRAPPER_HOOK_DIR  ${BORG_LOCAL}/VIRTUALENVWRAPPER_HOOK_DIR
+    .export VIRTUALENVWRAPPER_PYTHON    ${BORG_VENV}/bin/python
+    mkdir -p $WORKON_HOME
+    mkdir -p $PROJECT_HOME
     [[ -d $BORG_VENV ]] || venv.install;
     .exec source $BORG_VENV/bin/activate
     .source $ZDOTDIR/venv.sh;
@@ -54,6 +58,7 @@ function venv.install () {
 #  OMZSH
 #======================================================================
 function omzsh () {
+    .export ZSH                         ${BORG_LOCAL}/ZSH
     [[ -d $ZSH ]] || omzsh.install
     .export ZSH_THEME kafeitu # plugins must not contain virtualenv for prompt to show virtual_env)
     plugins=(git virtualenvwrapper)
